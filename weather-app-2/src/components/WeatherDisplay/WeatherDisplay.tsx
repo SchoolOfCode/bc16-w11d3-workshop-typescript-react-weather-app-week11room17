799b1506639c4db1f99ea15e9c8a99b16e8cc4f2
moose-hub <infamollagames@gmail.com>
2024-04-17 16:34:35 +0100

    restyle slightly

--- weather-app-2/src/components/WeatherDisplay/WeatherDisplay.tsx
+++ weather-app-2/src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -10,15 +10,29 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({  weatherData,  error,})
   if (error) return <div>Failed to load weather data</div>;
   if (!weatherData && !error) return <div>Loading...</div>;
   return (
-    <div className="WeatherDisplay">
-      <img src={weatherData?.current.condition.icon} alt="Weather Icon" />
-      <div className="weather-output">
-        <h2>{weatherData?.location.name}</h2>
-        <p>{weatherData?.location.localtime}</p>
-        <p>{weatherData?.current.temp_c} °C</p>
-      </div>
-    </div>
+    <>
+        <WeatherDisplayCard 
+            icon={weatherData?.current.condition.icon}
+            name={weatherData?.location.name} 
+            localTime={weatherData?.location.localtime}
+            tempC={weatherData?.current.temp_c}
+        />
+    </>
   );
 };
 
 export default WeatherDisplay;
+
+
+const WeatherDisplayCard = ({icon, name, localTime, tempC}) => {
+    return (
+        <div className="weather__display">
+          <img src={icon} alt="Weather Icon" />
+          <div className="weather-output">
+            <h2>{name}</h2>
+            <p>{localTime}</p>
+            <p>{tempC} °C</p>
+          </div>
+        </div>
+    )
+}
